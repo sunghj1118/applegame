f210f01de8a379afb7077fca007aaabb25834f70
성현준 <sunghj1118@gmail.com>
2023-04-01 02:28:20 +0900

    Succeeded on making random number grid of 17x10

--- applegame.cpp
+++ applegame.cpp
@@ -1,5 +1,33 @@
-#include <QVector>
+#include <iostream>
 
+using namespace std;
+
+int main() 
+{  
+    int s[10][17];
+	int i,j;
+    
+	for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 17; j++)
+        {
+            s[i][j] = (rand() % 9) + 1;
+        }
+    }
+	
+	
+	cout<<"Printing a 2D Array:\n";
+	for (i = 0; i < 10; i++)
+	{
+		for(j = 0; j < 17; j++)
+		{
+			cout<<"\t"<< s[i][j];
+		}
+		cout<<endl;
+	}
+}
+
+/*
 class Grid {
     public:
         Grid(const unsigned w = 8, const unsigned h = 8) : _w(w), _h(h) {
@@ -11,3 +39,4 @@ class Grid {
         QVector<> grid;
         unsigned _w, _h;
 }; 
+*/
